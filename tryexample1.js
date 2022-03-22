2e2cbc4cc1ff279ce886d10c78601196deab1a08
swapnilbawane <swapnilbawane08@gmail.com>
2022-03-22 14:29:23 +0530

    basic functionality done - check description
    basic functionality done now working on added functionality of taking custom numbers

--- tryexample1.js
+++ tryexample1.js
@@ -25,10 +25,17 @@ document.getElementById("volume-result").textContent = number1 + " litres = " + 
 document.getElementById("mass-result").textContent = number1 + " kilos = " + resultMassPounds + " pounds " + " | " + number1 + " pounds = " + resultMassKilos + " kilos "; 
 }
 
+// 20 meters = 65.616 feet | 20 feet = 6.096 meters 
+// 20 liters = 5.284 gallons | 20 gallons = 75.708 liters 
+//  20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos 
 
 
-function volumeltrGallon() 
-{ }
+function customvalue() 
+{ 
+let number2 = +prompt("enter a value");    
+alert("The number entered is " + number2);
+alert(typeof(number2));
+}
 
 function massKg() 
 { 
